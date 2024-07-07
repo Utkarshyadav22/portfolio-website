3093ffe88b9144a5f4755f4cc73d483aa3e2867a
Utkarshyadav22 <150050506+Utkarshyadav22@users.noreply.github.com>
2024-07-07 12:58:22 +0530

    Create Skills.js

---
+++ src/components/Skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <section id="skills" className="container">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>HTML & CSS</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
