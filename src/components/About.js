87ebbb3d19bc2958810ae3647be9cf1eb98e5ceb
Utkarshyadav22 <150050506+Utkarshyadav22@users.noreply.github.com>
2024-07-07 12:56:49 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about" className="container">
+      <h2>About Me</h2>
+      <p>Hello! I'm Utkarsh Yadav, a passionate full-stack developer.</p>
+    </section>
+  );
+};
+
+export default About;
