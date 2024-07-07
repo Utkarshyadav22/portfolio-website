b1244974a60fa100110565e57b979025f345364c
Utkarshyadav22 <150050506+Utkarshyadav22@users.noreply.github.com>
2024-07-07 12:55:46 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Utkarsh Yadav</h1>
+      <p>Full-Stack Developer</p>
+    </header>
+  );
+};
+
+export default Header;
